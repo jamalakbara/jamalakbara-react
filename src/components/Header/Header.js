b8f6bab42649b8ff53ae2c1b854d372532edf559
jamalakbara <jamalakbaralam@gmail.com>
2022-04-17 23:12:36 +0700

    seperate scss file with the intention of 'modularity' and then give animation to navigation bar when it scrolled

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,10 +2,27 @@ import Menu from '../Menu/Menu'
 import Logo from '../Logo/Logo'
 import './Header.scss'
 import Socials from '../Socials/Socials'
+import { useEffect, useState } from 'react'
 
 const Header = () => {
+  const [transparent, setTransparent] = useState(true)
+
+  const handleNavigation = () => {
+    if (window.scrollY > 25) {
+      setTransparent(false)
+    }
+    else{
+      setTransparent(true)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleNavigation)
+  }, [])
+  
+  
   return (
-    <nav className='nav'>
+    <nav className={`nav ${!transparent && 'nav--scrolled'}`}>
         <Logo />
         <Menu />
         <Socials />
